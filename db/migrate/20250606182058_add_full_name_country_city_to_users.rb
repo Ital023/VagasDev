74b17300a4d0fe4b6518c3fd2aac8a5535d03755
Italo <contadoitalo911@gmail.com>
2025-06-06 15:25:06 -0300

    feat: adding fields to user

---
+++ db/migrate/20250606182058_add_full_name_country_city_to_users.rb
@@ -0,0 +1,7 @@
+class AddFullNameCountryCityToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :country, :string
+    add_column :users, :city, :string
+  end
+end
